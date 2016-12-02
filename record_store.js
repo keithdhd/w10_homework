d9e892dfbe3a41a9bf84cf7233684ce66dfbd4b3
Bobby Ross <bobby1985uk@gmail.com>
2016-12-02 17:47:26 +0000

    add record to inventory method test passed

--- record_store.js
+++ record_store.js
@@ -3,6 +3,15 @@ var RecordStore = function(name, city){
   this.city = city;
   this.stockInventory = [];
   this.soldInventory = [];
+  this.sales = 0;
+  this.refunds = 0;
+};
+
+
+RecordStore.prototype = {
+  addInventory: function(record){
+    this.stockInventory.push(record);
+  }
 }
 
 module.exports = RecordStore;
--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -1,12 +1,15 @@
 var assert = require('assert');
 var RecordStore = require('../record_store');
+var Record = require('../record');
 
 describe("record store", function(){
 
   var recordStore1;
+  var record1;
 
   beforeEach(function(){
     recordStore1 = new RecordStore("Breaking Records", "Glasgow");
+    record1 = new Record("The Beatles", "Yellow Submarine", 9.99);
   });
 
   it("record store has name", function(){
@@ -23,5 +26,10 @@ describe("record store", function(){
 
   it("record store sold inventory starts at nil", function(){
     assert.equal(0, recordStore1.soldInventory.length);
+  });
+
+  it("can add record to inventory", function(){
+    recordStore1.addInventory(record1);
+    assert.equal(1, recordStore1.stockInventory.length);
   })
 })
